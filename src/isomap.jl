f9d988d48f154252ea3da36f53524e3dd7a7e706
wildart <wildart@gmail.com>
2019-06-04 00:15:39 -0400

    added OOS Isomap calculations

--- src/isomap.jl
+++ src/isomap.jl
@@ -4,29 +4,31 @@
 # J. B. Tenenbaum, V. de Silva and J. C. Langford, Science 290 (5500): 2319-2323, 22 December 2000
 
 #### Isomap type
-struct Isomap{T <: Real} <: AbstractDimensionalityReduction
-    k::Int
+struct Isomap{NN <: AbstractNearestNeighbors} <: AbstractDimensionalityReduction
+    nearestneighbors::NN
     model::KernelPCA
     component::AbstractVector{Int}
 
-    Isomap{T}(k::Int, model::KernelPCA) where T = new(k, model)
-    Isomap{T}(k::Int, model::KernelPCA, cc::AbstractVector{Int}) where T = new(k, model, cc)
+    Isomap{NN}(nn::NN, model::KernelPCA) where NN = new(nn, model)
+    Isomap{NN}(nn::NN, model::KernelPCA, cc::AbstractVector{Int}) where NN = new(nn, model, cc)
 end
 
 ## properties
 outdim(R::Isomap) = outdim(R.model)
 eigvals(R::Isomap) = principalvars(R.model)
-neighbors(R::Isomap) = R.k
+neighbors(R::Isomap) = R.nearestneighbors.k
 vertices(R::Isomap) = R.component
 
 ## show
-summary(io::IO, R::Isomap) = print(io, "Isomap(outdim = $(outdim(R)), neighbors = $(neighbors(R)))")
+summary(io::IO, R::Isomap{T}) where T =
+    print(io, "Isomap{$T}(outdim = $(outdim(R)), neighbors = $(neighbors(R)))")
 
 ## interface functions
 function fit(::Type{Isomap}, X::AbstractMatrix{T};
-             k::Int=12, maxoutdim::Int=2, knn=knn) where {T<:Real}
+             k::Int=12, maxoutdim::Int=2, nntype=BruteForce) where {T<:Real}
     # Construct NN graph
-    D, E = knn(X, k)
+    NN = fit(nntype, X, k)
+    D, E = knn(NN, X)
     G, C = largest_component(SimpleWeightedGraph(adjmat(D,E)))
 
     # Compute shortest path for every point
@@ -37,11 +39,22 @@ function fit(::Type{Isomap}, X::AbstractMatrix{T};
         DD[i,:] = dj.dists
     end
 
-    broadcast!(x->-x*x/2, DD, DD)
-    broadcast!((x,y)->(x+y)/2, DD, DD, DD') # remove roundoff error
-    M = fit(KernelPCA, DD, kernel=nothing, maxoutdim=maxoutdim)
+    M = fit(KernelPCA, dmat2gram(DD), kernel=nothing, maxoutdim=maxoutdim)
 
-    return Isomap{T}(k, M, C)
+    return Isomap{nntype}(NN, M, C)
 end
 
 transform(R::Isomap) = transform(R.model)
+
+function transform(R::Isomap, X::AbstractVecOrMat{T}) where {T<:Real}
+    n = size(X,2)
+    D, E = knn(R.nearestneighbors, X, self = true)
+    DD = gram2dmat(R.model.X)
+    G = zeros(size(R.model.X,2), n)
+    for i in 1:n
+        G[:,i] = minimum(DD[:,E[:,i]] .+ D[:,i]', dims=2)
+    end
+    broadcast!(x->-x*x/2, G, G)
+    transform!(R.model.center, G)
+    return projection(R.model)'*G
+end
